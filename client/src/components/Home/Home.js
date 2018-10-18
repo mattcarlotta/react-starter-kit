d3e8b2a7fb49d0096b5392861669b13e0e04586f
Matt <carlotta.matt@gmail.com>
2018-10-17 21:26:06 -0700

    Testing eslint

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -7,6 +7,7 @@ export default () => (
     <div className={logoContainer}>
       <img className={logoStyle} src={logo} alt="" />
       {test}
+      {weow}
       <h1 className={title}>Webpack React Boilerplate</h1>
       <h1 className={title}>Edit ./components and save to reload.</h1>
     </div>
