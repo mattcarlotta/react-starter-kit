815d31098c30f9de3932320dd3dcaffb6961d4c6
Matt <carlotta.matt@gmail.com>
2018-10-17 21:23:43 -0700

    Fix eslintignore

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,6 +6,7 @@ export default () => (
   <div className={app}>
     <div className={logoContainer}>
       <img className={logoStyle} src={logo} alt="" />
+      {test}
       <h1 className={title}>Webpack React Boilerplate</h1>
       <h1 className={title}>Edit ./components and save to reload.</h1>
     </div>
