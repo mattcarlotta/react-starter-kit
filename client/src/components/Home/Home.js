027e1c1f3b1b955b68bb3719a1909c595a76d39e
Matt <carlotta.matt@gmail.com>
2018-10-17 21:31:38 -0700

    Fixing lint on commit

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,8 +6,6 @@ export default () => (
   <div className={app}>
     <div className={logoContainer}>
       <img className={logoStyle} src={logo} alt="" />
-      {test}
-      {weow}
       <h1 className={title}>Webpack React Boilerplate</h1>
       <h1 className={title}>Edit ./components and save to reload.</h1>
     </div>
