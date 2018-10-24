f266f81f50e67cad348ec88496178ec179fcdc91
Matt <carlotta.matt@gmail.com>
2018-10-24 14:13:25 -0700

    Clean up left over comments

--- src/index.js
+++ src/index.js
@@ -14,10 +14,3 @@ if (module.hot) {
     render();
   });
 }
-
-// import React from 'react';
-// import { render } from 'react-dom';
-// import App from './root';
-// import './styles/globals/globals.scss';
-//
-// render(<App />, document.getElementById('root'));
