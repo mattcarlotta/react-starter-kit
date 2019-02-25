15f462c6a7ee6cb31eeba0928e2f6a9f13ad7bbf
Matt <carlotta.matt@gmail.com>
2019-02-25 13:09:11 -0800

    Updated packages on 02/25/2019 @ 01:09 PM

--- database/index.js
+++ database/index.js
@@ -1,32 +1,30 @@
 import bluebird from "bluebird";
 import mongoose from "mongoose";
-// const bluebird = require("bluebird");
-// const mongoose = require("mongoose");
-// import { DB } from "../envs";
+import { DB } from "../envs";
 
-const db = "mongodb://localhost/boilerplate-ssr-dev-db";
+export default () => {
+  mongoose.connect(DB, { useNewUrlParser: true }); // connect to our mongoDB database
+  mongoose.Promise = bluebird; // bluebird for mongoose promises
+  mongoose.connection.on(
+    "connected",
+    () => console.log(`\u001b[32mConnected to ${DB}\u001b[0m\n`) // console MongoDB connection established
+  );
+  mongoose.connection.on(
+    "disconnected",
+    () => console.log(`\n\u001b[36mDisconnected from ${DB}\u001b[0m`) // console MongoDB connection disconnected
+  );
 
-mongoose.connect(db, { useNewUrlParser: true }); // connect to our mongoDB database
-mongoose.Promise = bluebird; // bluebird for mongoose promises
-// mongoose.connection.on(
-//   "connected",
-//   () => console.log(`\u001b[32mConnected to ${db}\u001b[0m\n`) // console MongoDB connection established
-// );
-// mongoose.connection.on(
-//   "disconnected",
-//   () => console.log(`\n\u001b[36mDisconnected from ${db}\u001b[0m`) // console MongoDB connection disconnected
-// );
-//
-// mongoose.connection.on(
-//   "error",
-//   () => console.log(`\u001b[31mConnection error to ${db}\u001b[0m\n`) // console MongoDB connection error
-// );
-//
-// process.on("SIGINT", () => {
-//   mongoose.connection.close(() => {
-//     console.log(
-//       `\u001b[35mConnection was manually terminated from ${db}\u001b[0m` // console MongoDB manual disconnection
-//     );
-//     process.exit(0);
-//   });
-// });
+  mongoose.connection.on(
+    "error",
+    () => console.log(`\u001b[31mConnection error to ${DB}\u001b[0m\n`) // console MongoDB connection error
+  );
+
+  process.on("SIGINT", () => {
+    mongoose.connection.close(() => {
+      console.log(
+        `\u001b[35mConnection was manually terminated from ${DB}\u001b[0m` // console MongoDB manual disconnection
+      );
+      process.exit(0);
+    });
+  });
+};
