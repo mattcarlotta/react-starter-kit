7474a0351d7556bead58c776b12b225ee38070c9
Matt <carlotta.matt@gmail.com>
2019-02-25 13:03:12 -0800

    Updated packages on 02/25/2019 @ 01:03 PM

--- database/index.js
+++ database/index.js
@@ -1,27 +1,29 @@
 import bluebird from "bluebird";
 import mongoose from "mongoose";
-import { DB } from "../envs";
+// import { DB } from "../envs";
 
-mongoose.connect(DB, { useNewUrlParser: true }); // connect to our mongoDB database
+const db = "mongodb://localhost/boilerplate-ssr-dev-db";
+
+mongoose.connect(db, { useNewUrlParser: true }); // connect to our mongoDB database
 mongoose.Promise = bluebird; // bluebird for mongoose promises
 mongoose.connection.on(
   "connected",
-  () => console.log(`\u001b[32mConnected to ${DB}\u001b[0m\n`) // console MongoDB connection established
+  () => console.log(`\u001b[32mConnected to ${db}\u001b[0m\n`) // console MongoDB connection established
 );
 mongoose.connection.on(
   "disconnected",
-  () => console.log(`\n\u001b[36mDisconnected from ${DB}\u001b[0m`) // console MongoDB connection disconnected
+  () => console.log(`\n\u001b[36mDisconnected from ${db}\u001b[0m`) // console MongoDB connection disconnected
 );
 
 mongoose.connection.on(
   "error",
-  () => console.log(`\u001b[31mConnection error to ${DB}\u001b[0m\n`) // console MongoDB connection error
+  () => console.log(`\u001b[31mConnection error to ${db}\u001b[0m\n`) // console MongoDB connection error
 );
 
 process.on("SIGINT", () => {
   mongoose.connection.close(() => {
     console.log(
-      `\u001b[35mConnection was manually terminated from ${DB}\u001b[0m` // console MongoDB manual disconnection
+      `\u001b[35mConnection was manually terminated from ${db}\u001b[0m` // console MongoDB manual disconnection
     );
     process.exit(0);
   });
