75ddfa4c6bf95b96876ff0e61063c11b828e165d
Matt <carlotta.matt@gmail.com>
2019-03-09 10:34:26 -0800

    Update notes

--- config/envs.js
+++ config/envs.js
@@ -5,6 +5,6 @@
 module.exports = {
   currentDirectory: process.cwd(), // current working directory
   inDevelopment: process.env.NODE_ENV === 'development', // current environment
-  localIdentName: '[local]___[hash:base64:5]', //  class names will be defined in the DOM
+  localIdentName: '[local]___[hash:base64:5]', // how class names will be defined in the DOM
   PORT: process.env.PORT, // application's current port
 };
